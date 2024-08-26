664e7b6d6d834ce531cf2c9347d460d83033263c
jhon azcarate <trejos19>
2024-08-26 11:55:11 -0500

    a

---
+++ prueba.js
@@ -0,0 +1,56 @@
+//suma numeros
+// let numero1 = 10, numero2 = 15, numero3 = 20;
+// document.write(numero1+ numero2+ numero3);
+
+//cadena
+//numero += 1
+//document.write(numero)
+
+
+ //let nombre = prompt("dime tu nombre");
+   // alert(nombre);
+//let numero = 10;
+
+//saludo = "hola";
+//pregunta = "que haces";
+//frase = saludo + " " + pregunta;
+//document.write(frase);
+
+// let nombre = "gustavo"; 
+// frasenombre = "Hola soy "+nombre;
+// document.write(frasenombre);
+
+//boolan
+
+// let numero = false;
+// let numero2 = true;
+// document.write(numero);
+// document.write("<br>");
+// document.write(numero2);
+
+
+//arrays
+
+// let numeros = [10, 20, 30, 40, 50];
+// document.write(numeros);
+// document.write("<br>");
+// document.write(numeros[1]);
+
+
+let pc = {
+procesador :"inter core i5",
+    ram : "16",
+    almacenamiento : "SSD",
+    precio : "1200"
+};
+
+document.write(pc["procesador"])
+document.write("<br>");
+document.write(pc["ram"])
+document.write("<br>");
+document.write(pc["almacenamiento"])
+document.write("<br>");
+document.write(pc["precio"])
+
+
+
