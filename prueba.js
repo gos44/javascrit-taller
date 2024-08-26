60440e01ee1bf499b08fe3e09177ecdda670c591
jhon azcarate <trejos19>
2024-08-26 12:18:40 -0500

    3-

--- prueba.js
+++ prueba.js
@@ -37,20 +37,65 @@
 // document.write(numeros[1]);
 
 
-let pc = {
-procesador :"inter core i5",
-    ram : "16",
-    almacenamiento : "SSD",
-    precio : "1200"
-};
+// let pc = {
+// procesador :"inter core i5",
+//     ram : "16",
+//     almacenamiento : "SSD",
+//     precio : "1200"
+// };
 
-document.write(pc["procesador"])
-document.write("<br>");
-document.write(pc["ram"])
-document.write("<br>");
-document.write(pc["almacenamiento"])
-document.write("<br>");
-document.write(pc["precio"])
+// document.write(pc["procesador"])
+// document.write("<br>");
+// document.write(pc["ram"])
+// document.write("<br>");
+// document.write(pc["almacenamiento"])
+// document.write("<br>");
+// document.write(pc["precio"])
 
 
+// function calcularAreaTriangulo(base, altura) {
+//     return (base * altura) / 2;
+// }
 
+// console.log(calcularAreaTriangulo(5, 10)); // Muestra: 25
+// console.log(calcularAreaTriangulo(7, 14)); // Muestra: 49
+// console.log(calcularAreaTriangulo(6, 8));  // Muestra: 24
+
+
+// function verificarNumero(numero) {
+//     if (numero > 0) {
+//         console.log("El número es positivo");
+//     } else if (numero < 0) {
+//         console.log("El número es negativo");
+//     } else {
+//         console.log("El número es cero");
+//     }
+// }
+
+// verificarNumero(10);  // numero es positivo
+// verificarNumero(-5);  // numero es negativo
+// verificarNumero(0);   // numero es cero
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let contador = 10;
+
+// while (contador > 0) {
+//     console.log(contador);
+//     contador--;
+// }
+
+// let numero;
+// do {
+//     numero = prompt("Introduce un número mayor que 100:");
+// } while (numero <= 100 && numero);
+// console.log("Número válido: " + numero);
+
+// let nombres = ["Ana", "Luis", "María", "Juan"];
+
+// for (let nombre of nombres) {
+//     console.log(nombre);
+// }
